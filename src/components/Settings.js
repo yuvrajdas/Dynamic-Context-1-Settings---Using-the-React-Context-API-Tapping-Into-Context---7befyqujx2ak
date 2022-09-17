e24730442232e3eb2a77614a8689c0e92c0929c5
Yuvraj Das <48616872+yuvrajdas@users.noreply.github.com>
2022-09-17 12:22:15 +0530

    Update Settings.js

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -5,12 +5,16 @@ import { UserContext } from "../context/userContext"
 export const Settings = () => {
 
     // to be implemented in context
-    const { changeGreeting } = useContext(UserContext)
+    const { greeting, setGreeting } = useContext(UserContext)
+
+    const handleInput = (e) => {
+        setGreeting(e.target.value)
+    }
 
     return (
         <div style={{ border: '5px solid red', padding: '8px' }} id="settings">
             <h4>Settings</h4>
-            <input type={'text'} />
+            <input onChange={handleInput} value={greeting} type={'text'} />
         </div>
     )
 }
